67e1ec0c5b35bb2e123bd4a0f95024c8f5578116
Craig Johnson <craigdev937@gmail.com>
2024-04-16 12:12:38 -0400

    Updated App and Navbar components.

--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -3,16 +3,34 @@ import "./Navbar.css";
 import { Link, Outlet } from "react-router-dom";
 
 export const Navbar = () => {
+    const [burger_class, setBurgerClass] = React.useState("burger-bar unclicked");
+    const [menu_class, setMenuClass] = React.useState("menu hidden");
+    const [isMenuClicked, setIsMenuClicked] = React.useState(false);
+
+    // Toggle Burger Menu change
+    const updateMenu = () => {
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked");
+            setMenuClass("menu visible");
+        } else {
+            setBurgerClass("burger-bar unclicked");
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked);
+    };
+
     return (
         <React.Fragment>
-            <nav className="navbar">
-                <Link to={"/"}><h1>NAVBAR</h1></Link>
-                <ul>
-                    <Link to={"/"}>Films</Link>
-                    <Link to={"/shows"}>Shows</Link>
-                    <Link to={"/people"}>People</Link>
-                </ul>
-            </nav>
+            <header className="header">
+                <nav>
+                    <ul className="burger-menu" onClick={updateMenu}>
+                        <Link className={burger_class} to={"/"}>Films</Link>
+                        <Link className={burger_class} to={"/shows"}>Shows</Link>
+                        <Link className={burger_class} to={"/people"}>People</Link>
+                    </ul>
+                </nav>
+                <div className={menu_class}></div>
+            </header>
             <Outlet />
         </React.Fragment>
     );
